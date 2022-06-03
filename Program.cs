3d1b87eba4b9c4f11bb7ac8733c73e6892fe881e
Zzigol <zigolenko@rambler.ru>
2022-06-04 00:49:38 +0300

    начало 3 задачи

--- Program.cs
+++ Program.cs
@@ -15,12 +15,12 @@ namespace TextAnalysis
             var testsToRun = new string[]
             {
                //"TextAnalysis.SentencesParser_Tests",
-               "TextAnalysis.FrequencyAnalysis_Tests",
-               // "TextAnalysis.TextGenerator_Tests",
+               //"TextAnalysis.FrequencyAnalysis_Tests",
+                "TextAnalysis.TextGenerator_Tests",
             };
             new AutoRun().Execute(new[]
             {
-                "--stoponerror", // Останавливать после первого же непрошедшего теста. Закомментируйте, чтобы увидеть все падающие тесты
+                //"--stoponerror", // Останавливать после первого же непрошедшего теста. Закомментируйте, чтобы увидеть все падающие тесты
                 "--noresult",
                 "--test=" + string.Join(",", testsToRun)
             });
--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -1,15 +1,65 @@
 ﻿using System.Collections.Generic;
+using System.Text;
 
 namespace TextAnalysis
 {
     static class TextGeneratorTask
     {
-        public static string ContinuePhrase(
+       /* public static string TreeGrams(string ending, KeyValuePair<string,string> pair, int wordsCount)
+            
+        {
+            for (int i = 0; i < wordsCount; i++)
+            { 
+
+            }
+
+        }*/
+
+            public static string ContinuePhrase(
             Dictionary<string, string> nextWords,
             string phraseBeginning,
             int wordsCount)
         {
-            return phraseBeginning;
-        }
+            var builder = new StringBuilder();
+            string ending = null;
+            builder.Append(phraseBeginning);
+                foreach (var pair in nextWords)
+                {   
+                    for (int i = 0; i < wordsCount; i++) 
+                    {
+
+                        string[] array = phraseBeginning.Split(' ');
+                        if (array.Length >= 2)
+                        {
+                            ending = array[array.Length - 2] + " " + array[array.Length - 1];
+                        }
+                    if (ending == pair.Key)
+                        {
+                            builder.Append(" ");
+                            builder.Append(pair.Value);
+                            phraseBeginning = builder.ToString();
+                        }
+                    }
+                    for (int i = 0; i < wordsCount; i++)
+                    {
+                        string[] array = phraseBeginning.Split(' ');
+                        if (array[array.Length - 1] == pair.Key)
+                        {
+                            builder.Append(" ");
+                            builder.Append(pair.Value);
+                            phraseBeginning = builder.ToString();
+                        }
+
+                } 
+                }
+               
+            string result = builder.ToString();
+            return result;
+        }    
     }
 }
+
+            
+      
+   
+               

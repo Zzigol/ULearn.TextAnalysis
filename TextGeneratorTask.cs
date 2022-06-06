3a27e3c465269040f56c84ec5fa65bdae4246d62
Zzigol <zigolenko@rambler.ru>
2022-06-06 21:32:53 +0300

    3 задание решено

--- TextGeneratorTask.cs
+++ TextGeneratorTask.cs
@@ -9,49 +9,34 @@ namespace TextAnalysis
             Dictionary<string, string> nextWords,
             string phraseBeginning,
             int wordsCount)
-        {
-            var builder = new StringBuilder();
-            
-            builder.Append(phraseBeginning);
-            int i = wordsCount;
-            Found:
-            foreach (var pair in nextWords)
             {
-               // if ()
-               // {
-                    string[] array = phraseBeginning.Split(' ');
-                    if (array.Length >= 2 && array[array.Length - 2] + " " + array[array.Length - 1] == pair.Key && i > 0 && nextWords.ContainsKey(array[array.Length - 2] + " " + array[array.Length - 1]))
-                    {
-                        builder.Append(" ");
-                        builder.Append(pair.Value);
-                        phraseBeginning = builder.ToString();
-                        array = phraseBeginning.Split(' ');
-                        i--;
-                        if (i > 0) goto Found;
-                    }
-                    else if (array[array.Length - 1] == pair.Key && i > 0 && nextWords.ContainsKey(array[array.Length - 1]))
-                    {
-                        builder.Append(" ");
-                        builder.Append(pair.Value);
-                        phraseBeginning = builder.ToString();
-                        i--;
-                        if (i > 0 && nextWords.ContainsKey(array[array.Length - 1])) goto Found;
-                    }
-               // }
-                
-                /*else
+                var builder = new StringBuilder();
+                builder.Append(phraseBeginning);
+                int i = wordsCount;
+                Found:
+                string[] array = phraseBeginning.Split(' ');
+                if (array.Length >= 2 && 
+                    i > 0 && nextWords.ContainsKey(array[array.Length - 2] + " " + array[array.Length - 1]))
                 {
-                    if (i > 0) goto Found;
-                    else continue;
-
-                }*/
-            }  
-            return builder.ToString(); ;
+                    builder.Append(" " + nextWords[array[array.Length - 2] + " " + array[array.Length - 1]]);
+                    phraseBeginning = builder.ToString();
+                   // array = phraseBeginning.Split(' ');
+                    i--;
+                    goto Found;
+                }
+                else if (i > 0 && nextWords.ContainsKey(array[array.Length - 1]))
+                {
+                    builder.Append(" "+nextWords[array[array.Length - 1]]);
+                    phraseBeginning = builder.ToString();
+                    i--;
+                    goto Found;
+                }
+                else if (i > 0)
+                {
+                    i--;
+                    goto Found;
+                } 
+                return builder.ToString(); ;
             }    
-     }
-}
-
-            
-      
-   
-               
+    }
+}          
